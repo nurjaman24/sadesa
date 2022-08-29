b4c6baff001185c21fc73428680e84024d8bd9f0
nurjaman09 <nurjaman0901@gmail.com>
2022-08-29 20:55:50 +0700

    update

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/project/sadesa';
-// $config['base_url'] = 'https://meniti.id/sadesasukaasih/';
+// $config['base_url'] = 'http://localhost/project/sadesa';
+$config['base_url'] = 'https://meniti.id/sadesa/';
 
 
 /*
